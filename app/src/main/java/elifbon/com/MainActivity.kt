2c8b6c0e22138da84c484d3f74ed09c2e56dc1fd
Elif Boncuk <elifboncuk88@gmail.com>
2019-02-10 18:22:17 +0300

    Hello world by kotlin

---
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -0,0 +1,19 @@
+package elifbon.com
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(LOG_TAG, "Hello world")
+
+    }
+
+}
